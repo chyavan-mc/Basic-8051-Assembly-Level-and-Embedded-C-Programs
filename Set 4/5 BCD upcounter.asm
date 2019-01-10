bc841630da600e089b4f139b32e9f7db5ce62d24
Chyavan M C <chyavan.mc4@gmail.com>
2019-01-10 22:43:10 +0530

    Add files via upload

---
+++ Set 4/5 BCD upcounter.asm
@@ -0,0 +1,18 @@
+org 000h
+again:	mov a,#00h
+upc:	mov p0,a
+	acall delay
+	add a,#01
+	da a
+	cjne a,#00h,upc
+	sjmp again
+
+delay:	mov r1,#0FFh
+del1:	mov r2,#0FFh
+del2: 	mov r3,#0FFh
+del3:	djnz r3,del3
+	djnz r2,del2
+	djnz r1,del1
+	ret
+
+	end
