a3705c447c1ca88b67055ddb8268db1aebaced85
Chyavan M C <chyavan.mc4@gmail.com>
2019-01-22 08:56:48 +0530

    Add files via upload

---
+++ Set 7/1 A Sqaure wave Mode0.asm
@@ -0,0 +1,11 @@
+org 000h
+	mov tmod,#00h
+repeat:	mov tl0,#00h
+	mov th0,#00h	;(tl0,th0)=(00,00) for max delay and (FF,FF) for minimum delay
+	setb tr0
+wait:	jnb tf0,wait
+	clr tr0
+	clr tf0
+	cpl p1.5
+	sjmp repeat
+	end
